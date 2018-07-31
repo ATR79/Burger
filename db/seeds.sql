61cddf062c75da5f9ea08857b8f0d400353d2ffa
Anna Rutherford <anna.t.rutherford@gmail.com>
2018-07-30 20:52:43 -0400

    directory setup

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Hawaiian');
+INSERT INTO burgers (burger_name) VALUES ('Monster');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swiss', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('All American', true);
